0e2b3ebfc8b5e4213fd3cf4942af9d13d4aeaeb7
Imtiajul Islam <iimtiajul@gmail.com>
2018-03-16 01:14:26 +0600

    Add files via upload

---
+++ Problem Set 3 Loop/3.3.0 Factorial.py
@@ -0,0 +1,7 @@
+n =  int(input())
+
+fac = 1
+for i in range(1, 1+n):
+    fac = fac *i
+    #print(fac, end=' ')
+print(fac)
---
+++ Problem Set 3 Loop/3.3.0.Summation.py
@@ -0,0 +1,7 @@
+n =  int(input())
+
+sum = 0
+for i in range(1, 1+n):
+    sum = sum +i
+    #print(sum, end=' ')
+print(sum)
